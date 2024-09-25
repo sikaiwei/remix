94a3baad95d96799db2e14358deb4881e73e514c
sikaiwei <sikaiwei0@163.com>
2024-09-25 23:49:34 +0800

    写入槽

--- tests/WrongWayToWriteAnUpgrade.sol
+++ tests/WrongWayToWriteAnUpgrade.sol
@@ -43,23 +43,64 @@ contract CounterV2 {
 contract BuggyProxy{
     address public implementation;
     address public admin;
-    uint256 public count;
+    // uint256 public count;
 
     constructor() {
         admin = msg.sender;
     }
 
-    function _delegate() private {
-        (bool ok, bytes memory res) = implementation.delegatecall(msg.data);
-        require(ok, "delegatecall failed");
+    // function _delegate() private {
+    //     (bool ok, bytes memory res) = implementation.delegatecall(msg.data);
+    //     require(ok, "delegatecall failed");
+    // }
+
+    function _delegate(address _implementation) private {
+        assembly {
+            // Copy msg.data.We take full control of menory in this inline assembty
+            //block becouse it will not retum to Solldity.code.Me ovenrite the
+            // Solidity scratch pad at hemory position 0.
+            // calldatacopy(t,f,s）-copy s bytcs fron calldata at position f to wem at position 
+            //calldatasize()-sizr of coll dota in bytes
+            calldatacopy(0, 0, calldatasize())
+
+            //Call the implenentation.
+            //out and outslze are B because we don't know the size yet.
+
+            //delegatecVg,a,in,insize,out.ousize）-
+            //-coll coract aaddress a
+            //-wdth input nem[in（in+insize）)
+            //-praviding g gas
+            //-and ootout arca Memlout_(out+outsizei)
+            //-returningo on crror (eg.out of gas）ond 1 on success
+            let result := 
+                delegatecall(gas(), _implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            //returndatacopy(t,f,s)-copy s bytes from returndata at position f to nen at position t
+            //returndatasizel)-sizc of the last returndata
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegetecall returns 0 on error.
+            case 0 {
+                //revert(p,s）-end execution,revert state changes,return data mem[p_（p+s))
+                revert(0, returndatasize())
+            }
+            default {
+                //return(p,s)-end execution,return data mem[p-(p+s))
+                return (0, returndatasize())
+            }
+
+        }
     }
 
+
     fallback() external payable {
-        _delegate();
+        _delegate(implementation);
     }
 
     receive() external payable {
-        _delegate();
+        _delegate(implementation);
     }
 
     function upgradeTo(address _implementation) external {
